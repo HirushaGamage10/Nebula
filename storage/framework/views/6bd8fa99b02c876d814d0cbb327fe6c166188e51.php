37977d1ef3ee0d34106c14d6d06426dfdbaf1138
Thisali2000 <lbthisaliravindya@gmail.com>
2025-12-15 10:39:16 +0530

    sidebar issues fixed

---
+++ storage/framework/views/6bd8fa99b02c876d814d0cbb327fe6c166188e51.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('title', 'NEBULA | Student Counselor Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('dashboards.student_counselor_content', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('inc.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\thisali\Desktop\thisali\Nebula\resources\views/dashboards/student_counselor.blade.php ENDPATH**/ ?>
