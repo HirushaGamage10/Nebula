526c2675381e7c18addde73b9f177377388a2f4c
Thisali2000 <lbthisaliravindya@gmail.com>
2025-12-15 10:39:16 +0530

    sidebar issues fixed

---
+++ storage/framework/views/f88bf8a3fe4c3b7248919e1a0f0cc3faf13d0ee2.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('title', 'NEBULA | Program Administrator Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('adminl1.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('inc.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\thisali\Desktop\thisali\Nebula\resources\views/dashboards/admin_l1.blade.php ENDPATH**/ ?>
