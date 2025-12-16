5ace5b43170bf0d9cf7cfb3b95032a0f132a21ec
Savindu Fernando <savindunipun30@gmail.com>
2025-12-16 11:50:00 +0530

    Routes Changes Fixed for Course Change

---
+++ storage/framework/views/ddef3ada1a9f70fd94e0f2376373204f.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('title', 'NEBULA | Marketing Manager Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('dashboards.marketing_manager_content', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('inc.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\SLT\Welisara\Nebula\resources\views/dashboards/marketing_manager.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/eb2da279bab3d23e54abe9f9fe4afb84.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('title', 'NEBULA | DGM Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('dashboards.dgmdashboard_content', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('inc.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\SLT\Welisara\Nebula\resources\views/dashboards/dgmdashboard.blade.php ENDPATH**/ ?>
