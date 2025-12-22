339ef1c46788ef2186888e2a9afba33f49bf2037
Thisali2000 <lbthisaliravindya@gmail.com>
2025-12-22 15:26:14 +0530

    Student registration issue fixed

---
+++ storage/framework/views/b964ddecbc384d23ca3fe8ead086d8926eea0015.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('title', 'NEBULA | Marketing Manager Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('dashboards.marketing_manager_content', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('inc.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\thisali\Desktop\thisali\Nebula\resources\views/dashboards/marketing_manager.blade.php ENDPATH**/ ?>
